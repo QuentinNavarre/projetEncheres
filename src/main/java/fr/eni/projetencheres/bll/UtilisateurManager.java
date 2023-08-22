35589cc63e2fc8c64d88f2c8573edb09364960b4
CharlesVanzetta <charles.vanzetta@gmail.com>
2023-08-22 18:23:57 +0200

    couche BLL

---
+++ src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
@@ -0,0 +1,38 @@
+package fr.eni.projetencheres.bll;
+
+import fr.eni.projetencheres.BusinessException;
+import fr.eni.projetencheres.bo.Utilisateur;
+import fr.eni.projetencheres.dal.DAOFactory;
+
+// TODO import fr.eni.projetencheres.dal.UtilisateurDAO;
+public class UtilisateurManager {
+
+	private UtilisateurDAO utilisateurDao;
+	private static UtilisateurManager instance;
+
+	private UtilisateurManager() {
+		utilisateurDAO = DAOFactory.getUtilisateurDAO();
+	}
+
+	public static UtilisateurManager getInstance() {
+
+		if (instance == null) {
+			instance = new UtilisateurManager();
+		}
+		return instance;
+	}
+
+	public void seConnecter(String identifiant, String mdp) throws BuisinessException {
+		Utilisateur user = null;
+		if (identifiant.contains("@"))
+			user = utilisateurDAO.getUtilisateurByMail(identifiant);
+
+		utilisateurDAO.getUtilisateurByPseudo(identifiant);
+
+		if (user == null || !mdp.equals(user.getMot_de_passe)) {
+			BusinessException be = new BusinesseException();
+			be.ajouterErreur(codeResultatBLL.IDENTIFANT_KO);
+			throw be;
+		}
+	}
+}
