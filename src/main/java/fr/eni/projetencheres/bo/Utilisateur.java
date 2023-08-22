68687d7bed605b073c38fa281ccc81a91e85f126
QuentinNavarre <q.navarre@laposte.net>
2023-08-22 11:20:06 +0200

    Ajout classe Utilisateur

---
+++ src/main/java/fr/eni/projetencheres/bo/Utilisateur.java
@@ -0,0 +1,16 @@
+package fr.eni.projetencheres.bo;
+
+public class Utilisateur {	
+	private Integer utilisateurId;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String code_postal;
+	private String ville;
+	private String mot_de_passe;
+	private int credit;
+	private Boolean administrateur;
+}
