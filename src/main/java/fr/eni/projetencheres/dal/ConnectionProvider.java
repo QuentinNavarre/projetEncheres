047a13068c066e5073e3f4f62def103a0064d680
kiClem <martin.clement.bts@gmail.com>
2023-08-23 09:54:44 +0200

    ajout .bo + .dal

--- src/main/java/fr/eni/projetencheres/dal/ConnectionProvider.java
+++ src/main/java/fr/eni/projetencheres/dal/ConnectionProvider.java
@@ -8,7 +8,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-abstract class ConnectionProvider {
+public class ConnectionProvider {
 	private static DataSource dataSource;
 	
 	/**
