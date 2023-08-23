b783a15addf122c592dda98f9ec0ee6c749e5d71
CawilleR <courrieldecamille@gmail.com>
2023-08-23 08:38:07 +0200

    modifications ServletSeConnecter

--- src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
+++ src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.projetencheres.BusinessException;
+import fr.eni.projetencheres.bll.UtilisateurManager;
+
 /**
  * Servlet implementation class ServletSeConnecter
  */
@@ -26,8 +29,15 @@ public class ServletSeConnecter extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String identifiant = request.getParameter("login");
+		String mdp = request.getParameter("mdp");
+		try {
+			UtilisateurManager.getInstance().seConnecter(identifiant, mdp);
+			response.sendRedirect("https://www.google.com/");
+		} catch (BusinessException e) {
+			request.setAttribute("listesCodesErreur", e.getListeCodesErreur());
+			request.getRequestDispatcher("WEB-INF/jsp/projetEncheres.jsp").forward(request, response);
+		}
 	}
 
 }
