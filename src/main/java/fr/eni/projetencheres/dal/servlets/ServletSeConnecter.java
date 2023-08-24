1c78204e95cc661fd4c70b4d79fc03d068cff319
CawilleR <courrieldecamille@gmail.com>
2023-08-24 12:01:25 +0200

    Modification accueil.jsp, ServletSeConnecter + ajout fichier css

--- src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
+++ src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.projetencheres.BusinessException;
 import fr.eni.projetencheres.bll.UtilisateurManager;
@@ -24,6 +25,7 @@ public class ServletSeConnecter extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		request.getRequestDispatcher("WEB-INF/jsp/projetEncheres.jsp").forward(request, response);
 	}
 
@@ -33,7 +35,11 @@ public class ServletSeConnecter extends HttpServlet {
 		String mdp = request.getParameter("mdp");
 		try {
 			UtilisateurManager.getInstance().seConnecter(identifiant, mdp);
-			response.sendRedirect("https://www.google.com/");
+			HttpSession session = request.getSession(); // ++
+			session.setAttribute("identifiant", identifiant);
+	        session.setAttribute("userLoggedIn", true);
+	        response.sendRedirect(request.getContextPath() + "/encheres");
+
 		} catch (BusinessException e) {
 			request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
 			request.getRequestDispatcher("WEB-INF/jsp/projetEncheres.jsp").forward(request, response);
