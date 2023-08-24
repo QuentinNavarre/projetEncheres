64994dc307b491c84b396feb8bee9e3d182dfa13
CharlesVanzetta <charles.vanzetta@gmail.com>
2023-08-24 12:18:08 +0200

    inscription et hashage.

--- src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
+++ src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeConnecter.java
@@ -1,6 +1,7 @@
 package fr.eni.projetencheres.dal.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import fr.eni.projetencheres.BusinessException;
 import fr.eni.projetencheres.bll.UtilisateurManager;
 
-/**
- * Servlet implementation class ServletSeConnecter
- */
 @WebServlet("/SeConnecter")
 public class ServletSeConnecter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public ServletSeConnecter() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -31,13 +28,22 @@ public class ServletSeConnecter extends HttpServlet {
 			throws ServletException, IOException {
 		String identifiant = request.getParameter("login");
 		String mdp = request.getParameter("mdp");
+
 		try {
-			UtilisateurManager.getInstance().seConnecter(identifiant, mdp);
-			response.sendRedirect("https://www.google.com/");
+			boolean authentifie = UtilisateurManager.getInstance().authentifier(identifiant, mdp);
+
+			if (authentifie) {
+				// L'utilisateur est authentifié, redirigez-le vers la page d'accueil
+				response.sendRedirect("PageAccueil.jsp");
+			} else {
+				// L'authentification a échoué, renvoyez un message d'erreur à l'utilisateur
+				request.setAttribute("erreur", "Nom d'utilisateur ou mot de passe incorrect");
+				request.getRequestDispatcher("WEB-INF/jsp/projetEncheres.jsp").forward(request, response);
+			}
 		} catch (BusinessException e) {
+			// Gérez l'exception BusinessException
 			request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
 			request.getRequestDispatcher("WEB-INF/jsp/projetEncheres.jsp").forward(request, response);
 		}
 	}
-
 }
