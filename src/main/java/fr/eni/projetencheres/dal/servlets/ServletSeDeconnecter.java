85204e0550f59d329cb18a1e419d98b4c5ffdc43
kiClem <martin.clement.bts@gmail.com>
2023-08-25 09:48:33 +0200

    bouton deconnection

--- src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeDeconnecter.java
+++ src/main/java/fr/eni/projetencheres/dal/servlets/ServletSeDeconnecter.java
@@ -7,42 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 /**
  * Servlet implementation class ServletSeDeconnecter
  */
 @WebServlet("/ServletSeDeconnecter")
 public class ServletSeDeconnecter extends HttpServlet {
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletSeDeconnecter() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		HttpSession s=request.getSession(false);
-		s.invalidate();
-		request.setAttribute("mgs","Vous etes deconnecte de votre session");
-		request.setAttribute("typemsg", "success");
-		request.
-		request.getRequestDispatcher("WEB-INF/jsp/ServletSeConnecter.jsp").forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        HttpSession session = request.getSession(false);
 
+        if (session != null) {
+            session.invalidate();
+        }
+
+        // Redirection vers la page de déconnexion
+        response.sendRedirect("Deconnection.jsp");
+    }
 }
