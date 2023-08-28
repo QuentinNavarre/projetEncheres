f66663e8ba18389b369cb7cee93cc3824bd85927
CharlesVanzetta <charles.vanzetta@gmail.com>
2023-08-28 15:19:11 +0200

    enchere en BO

---
+++ src/main/java/fr/eni/projetencheres/bo/Enchere.java
@@ -0,0 +1,46 @@
+package fr.eni.projetencheres.bo;
+
+import java.sql.Timestamp;
+
+public class Enchere {
+
+	private int noUtilisateur;
+	private int noArticle;
+	private Timestamp dateEnchere;
+	private int montantEnchere;
+
+	// Les getters et setters
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public Timestamp getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(Timestamp dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+}
