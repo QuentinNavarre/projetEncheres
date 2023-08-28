442326d711bc14b4d059983f59f6cbd468ff151d
QuentinNavarre <q.navarre@laposte.net>
2023-08-28 16:20:28 +0200

    .

--- src/main/java/fr/eni/projetencheres/dal/jdbc/ArticleDAOJdbcImpl.java
+++
@@ -1,7 +0,0 @@
-package fr.eni.projetencheres.dal.jdbc;
-
-public class ArticleDAOJdbcImpl {
-	private static final String SELECT_ARTICLES_BY_CATEGORY = "SELECT no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie FROM ARTICLES_VENDUS WHERE no_categorie = ?;";
-
-
-}
